22a6aeb67d4ab3b99f26dadaf33dddbcb9285ba9
Rahul Shembdekar <OPO113177@onepointone.in>
2024-12-05 17:56:55 +0530

    Add Program fun

---
+++ Process/Program.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Program : System.Web.UI.Page
+{
+    DL_Encrpt drc = new DL_Encrpt();
+    Dl_Connection con = new Dl_Connection();
+    public string ConnSTR = ConfigurationManager.ConnectionStrings["SuperDataBase"].ConnectionString;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindAccountDetails();
+        }
+    }
+
+    public void BindAccountDetails()
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(UserInfo.Dnycon))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("USP_Fill_Dropdown", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Mode", "Select_Location_Master");
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+                    adpt.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        LocationDropDown.DataSource = dt;
+                        LocationDropDown.DataTextField = "Location";
+                        LocationDropDown.DataValueField = "ID";
+                        LocationDropDown.DataBind();
+                        LocationDropDown.Items.Insert(0, new System.Web.UI.WebControls.ListItem("Select", ""));
+                    }
+                    else
+                    {
+                        LocationDropDown.Items.Insert(0, new System.Web.UI.WebControls.ListItem("No Accounts Available", ""));
+                    }
+                }
+            }
+        }
+        catch (SqlException ex)
+        {
+            // Log error and show a message to the user
+            Response.Write("Database error: " + ex.Message);
+        }
+    }
+}
